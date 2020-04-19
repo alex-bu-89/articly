c93a5f8a9e03d23c0afffa73625734bed520dbe4
Alex Buyanov <alex.bu.89@gmail.com>
2020-04-19 18:07:20 +0200

    Working on background script

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,3 +1,5 @@
+console.log('------content script');
+
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     if (msg.color) {
         console.log('Receive color = ' + msg.color);
