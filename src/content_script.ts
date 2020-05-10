8808983bf3418e0cdff5b6d50e502e40765239ac
Alexander Buyanov <alex.bu.89@gmail.com>
2020-05-10 16:55:39 +0200

    Get selection on click; WIP

--- src/content_script.ts
+++ src/content_script.ts
@@ -5,25 +5,23 @@ function translateSelection(selection: string, callback: (payload: any) => void)
     chrome.runtime.sendMessage(
         { type: 'translateSelection', payload: { selection } },
         (response) => {
-            console.log(response.payload);
-            callback ?? callback(response.payload)
+            callback && callback(response.payload)
         }
     );
 }
 
 /**
- * Init ondblclick event
+ * Init hightlight text event
  */
 document.addEventListener('mouseup', (event) => {
     const selection = getSelectionHTML();
-    console.log('------, test', selection);
+
+    if (selection.length < 1) return;
+
+    console.log('------ selection', selection);
 
     translateSelection(selection, (payload) => {
         console.log('------ response', payload);
     });
 
 }, false);
-
-// document.body.addEventListener('')
-
-// chrome.eve
